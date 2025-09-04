3372ca16e5853ef3417e107ac0aee6764efea71d
Wagner <65842719+wagnersk@users.noreply.github.com>
2025-09-04 03:20:44 +0100

    Initial Commit

---
+++ ReminderTests/ReminderTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderTests.swift
+//  ReminderTests
+//
+//  Created by Wagner Sobreira on 9/4/25.
+//
+
+import Testing
+@testable import Reminder
+
+struct ReminderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
